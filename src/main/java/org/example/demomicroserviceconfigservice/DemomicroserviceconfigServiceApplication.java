e70978af7d4c7fd2c26a7a4df1d0b9e3e4cfb5c0
looksaw <xdu2814031084@gmail.com>
2025-04-06 11:47:06 +0800

    first commit

---
+++ src/main/java/org/example/demomicroserviceconfigservice/DemomicroserviceconfigServiceApplication.java
@@ -0,0 +1,13 @@
+package org.example.demomicroserviceconfigservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemomicroserviceconfigServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemomicroserviceconfigServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/demomicroserviceconfigservice/DemomicroserviceconfigServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.demomicroserviceconfigservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemomicroserviceconfigServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
